385335086e488f85613195fd024893ad01c14d7c
Luis Alejandro <baphomety.black@gmail.com>
2020-12-18 02:27:39 -0600

    Blog dinamic add

--- blog/applications/entrada/managers.py
+++ blog/applications/entrada/managers.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Q
 
 class EntryManager(models.Manager):
     """ Manager del modelo Entry """
@@ -21,3 +22,16 @@ class EntryManager(models.Manager):
         return self.filter(
             public=True,
         ).order_by('-created')[:6]
+
+    def buscar(self, kword, categoria):
+        # devuelve una lista de entradas segun unas palabras clave o categoria
+        if categoria:
+            return self.filter(
+                Q(title__icontains = kword) | Q(resume__icontains = kword),
+                category__short_name = categoria,
+                public = True,
+            ).order_by('-created')
+        
+        return self.filter(
+                Q(title__icontains = kword) | Q(resume__icontains = kword)
+            ).order_by('-created')
--- blog/applications/entrada/views.py
+++ blog/applications/entrada/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+# Models
+from .models import Entry, Category
 
-# Create your views here.
+class EntryListView(ListView):
+    template_name = "entrada/lista.html"
+    context_object_name = "entradas"
+    paginate_by = 2
+    
+    def get_context_data(self, **kwargs):
+        context = super(EntryListView, self).get_context_data(**kwargs)
+        context["categorias"] = Category.objects.all()
+        return context
+    
+    
+    def get_queryset(self):
+        categoria = self.request.GET.get("categoria",'')
+        kword = self.request.GET.get("kword",'')
+        # Busqueda
+        result = Entry.objects.buscar(kword, categoria)
+        return result
+    
